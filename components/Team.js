cca01901f9c2c5d2754a05a9932d254c9cc248f9
ckc1373 <ccourtne@hamilton.edu>
2018-08-27 23:34:34 -0400

    added recent runs, add run, and team page

---
+++ components/Team.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  AsyncStorage,
+  ListView,
+  RefreshControl
+} from 'react-native';
+import { Button, Header, List, ListItem, Divider, Text as Title } from 'react-native-elements'
+
+
+import mainStyles from '../mainStyles.js'
+
+
+
+export default class Team extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  componentDidMount(){
+    // let user;
+    // AsyncStorage.getItem('token').then((result) => {
+    //   return JSON.parse(result)})
+    // .then((value) => {
+    //   if(value && value.userId){
+    //     user = value.userId
+    //     this.setState({
+    //       userId: value.userId,
+    //     })
+    //   }
+  }
+
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+        <Header
+          leftComponent={{ type: 'entypo', icon: 'home', color: '#fff', onPress: () => this.props.navigation.push('Home')}}
+          centerComponent={{ text: 'Team', style: { color: '#fff' } }}
+          outerContainerStyles={mainStyles.darkBackground}
+        />
+  </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+
+});
